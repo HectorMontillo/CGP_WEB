c3dacfda2314eee796800c9fe6a409828f61ca11
Hector Montillo <hector.montillo@utp.edu.co>
2019-10-29 16:06:20 -0500

    Agregue CORS

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const server = require("http").Server(app);
+const cors = require("cors");
 const bodyParser = require("body-parser");
 
 const routes = require("./network/routes");
@@ -10,14 +11,14 @@ const db = require("./db");
 db(
   "mongodb+srv://user:user1234@cluster0-lsrfs.mongodb.net/cgpweb?retryWrites=true&w=majority"
 );
-
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/app", express.static("public"));
 
 app.get("/", (req, res) => {
-    res.send("Welcome to a basic express App");
+  res.send("Welcome to a basic express App");
 });
 
 routes(app);
