1900dbef660addec3f526ef0759032e4d1c8ee5c
Hector Montillo <hector.montillo@utp.edu.co>
2019-11-05 15:22:43 -0500

    Agregando OPTIONS

--- server.js
+++ server.js
@@ -17,14 +17,19 @@ const routes = require("./network/routes");
 app.use("/app", express.static("public"));
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  // Set custom headers for CORS
   res.header(
     "Access-Control-Allow-Headers",
-    "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
+    "Content-type,Accept,X-Custom-Header"
   );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
+  return next();
 });
 
 app.get("/", (req, res) => {
