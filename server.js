bbff190b52236076980ac62670398e1bdcc08b94
Hector Montillo <hector.montillo@utp.edu.co>
2019-11-05 15:10:49 -0500

    Reparando cors

--- server.js
+++ server.js
@@ -16,6 +16,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 const routes = require("./network/routes");
 app.use("/app", express.static("public"));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.get("/", (req, res) => {
   res.send("Welcome to a basic express App");
 });
